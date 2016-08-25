8bf8bf5fa4a78ea4420769f22df2dc231a0575f1
jocelynlozano <jocelynlozanocarbajal@gmail.com>
2016-08-25 15:10:07 -0500

    subiendo ejercicio

---
+++ app.js
@@ -0,0 +1,4 @@
+var numero1 = parseInt(prompt ("Ingrese su primer numero"));
+var numero2 = parseInt(prompt ("Ingrese su segundo numero"));
+var numero3 = parseInt(prompt ("Ingrese su tercer numero"));
+	window.alert("Su promedio es " + (numero1 + numero2 + numero3)/3);
